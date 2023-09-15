a205b6a7050f8f06d1b4d8e176f38eb49f793496
Sander <sander.geurts.1@gmail.com>
2023-09-15 18:56:01 +0200

    introduced a data race :)

--- Assignment 2/numerical_integration.cpp
+++ Assignment 2/numerical_integration.cpp
@@ -19,35 +19,52 @@ float func(float x)
   return 4 / (1 + pow(x, 2));
 }
 
-void trapArea(int i, float start, float stepsize, float *result)
+void trapArea(int i, float start, float stepsize, float *result, bool *flag)
 {
   float a = start + i * stepsize;
   float b = start + (i + 1) * stepsize;
   *result = (func(a) + func(b)) * ((b - a) / 2);
+  *flag = false;
 }
 
 int main(int argc, char *argv[])
 {
-  int n = 20;
-  int threadCount = 5;
-  
+  int n = 5;
+  int threadCount = 2;
+  bool flags[threadCount];
+  std::thread *threads = new std::thread[threadCount];
+
   float startx = 0;
   float stopx = 1;
   float stepsize = (stopx - startx) / n;
 
   float values[n];
-  std::thread *ta = new std::thread[threadCount];
-  // bool flags[threadCount];
 
+  bool assigned;
   for (int i = 0; i < n; i++)
   {
-    ta[i] = std::thread(&trapArea, i, startx, stepsize, &values[i]);
+    while (!assigned) // while the index is not assigned
+    {
+      for (int t = 0; t < threadCount; t++) // loop over all threads
+      {
+        if (!flags[t]) // assign if available
+        {
+          flags[t] = true;
+          threads[t] = std::thread(&trapArea, i, startx, stepsize, &values[i], &flags[t]);
+          assigned = true;
+
+          std::cout << "index " << i << "assigned to thread "<< t << std::endl;
+
+        }
+      }
+    }
+
     // std::cout << a << " " << b << " " << values[i] << std::endl;
   }
 
-  for(int i = 0; i < n; i++)
+  for (int t = 0; t < threadCount; t++) // wait for threads to finish
   {
-    ta[i].join();
+    threads[t].join();
   }
 
   float totalArea = 0;
